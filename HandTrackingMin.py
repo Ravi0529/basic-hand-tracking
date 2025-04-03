83da6c234dd3f62f730748cf9c36b8d05e1979b2
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-04-03 13:42:02 +0530

    landmark x, y tracking

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -19,13 +19,20 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print(id, cx, cy)
+                cv2.circle(img, (cx, cy), 7, (0, 255, 0), cv2.FILLED)
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv2.putText(
+        img, f"FPS: {int(fps)}", (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3
+    )
 
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord("q"):
